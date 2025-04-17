a09ac168f7d2414b3626b1392a21e4621609df3b
Faiz100-coder <faizanahmed4355@gmail.com>
2025-04-17 15:01:38 +0530

    v2

---
+++ 8march/funinhri.js
@@ -0,0 +1,31 @@
+// use function constructor or function inheritance to solve the same problem
+
+// this is parent class
+
+// function superType() {
+//     this.name = "faizan"
+
+// }
+// superType.prototype.getname = function () {
+//     console.log("parent", this.name);
+// }
+
+// // this is the child class
+
+// function subType() {
+
+//     this.age = 21;
+// }
+// subType.prototype = new superType()
+// subType.prototype.getage = function () {
+//     console.log("child age : ", this.age);
+
+// }
+// obj1 = new subType()
+// obj1.getname();
+// obj1.getage();
+
+// obj2 = new superType()
+// obj2.getname();
+// obj2.getage();
+
